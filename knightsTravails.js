eeac54875d4e0730af0e005201c1dab93a4040df
GiovanniTirone <tironegiovanni4@gmail.com>
2022-10-08 17:55:21 +0200

    Remove the comments

--- knightsTravails.js
+++ knightsTravails.js
@@ -28,28 +28,8 @@ for(let a=0; a<8; a++){
     }
 }
 
-/*
-for(let a=0; a<8; a++){ 
-    for(let b=0; b<8; b++){
-        console.log("Possible moves for the box (" + a + "," + b +") : ");
-        knigthMoves[a+","+b].forEach(move => console.log(move));
-    }
-}
-*/
-//MAX DEPTH = 63
 
-const start = {
-    startingBox : [],
-    nextMoves : [], 
-}
-
-const box = {
-    box : [],
-    nextMoves : [],
-}
-
-
-//insert the box in array form
+//insert the boxes in array form
 
 const minPath = (startBox,endBox) => {
  
@@ -57,7 +37,7 @@ const minPath = (startBox,endBox) => {
 
     const minPathRec = (startRow,startCol,endRow,endCol,path,depth,justFoundDepths) => {
         let minFoundDepth = Math.min(...justFoundDepths);
-        if (depth>63 || depth>justFoundDepths){return} //rivedere
+        if (depth>63 || depth>minFoundDepth){return} //rivedere
         const startBox = knigthMoves[startRow+","+startCol];
         startBox.forEach(move =>{
             const newPath = path.map(box => box); 
@@ -71,50 +51,14 @@ const minPath = (startBox,endBox) => {
         });
     }
 
-    minPathRec(startBox[0],startBox[1],endBox[0],endBox[1],[[startBox[0],startBox[1]]],0);
+    minPathRec(startBox[0],startBox[1],endBox[0],endBox[1],[[startBox[0],startBox[1]]],0,[]);
 
-    //const possiblePathsLengths = possiblePaths.map(path => { return {"path": path, "pathLength": path.length} });
-    //possiblePathsLengthsNoDuplicates =  removeDuplicates(possiblePathsLengths);
-    //mergeSort(possiblePathsLengthsNoDuplicates); 
-    //return possiblePathsLengthsNoDuplicates[0]["path"];
+    const possiblePathsLengths = possiblePaths.map(path => { return {"path": path, "pathLength": path.length} });
     possiblePathsLengths.sort((a,b)=> a.pathLength - b.pathLength);
     return possiblePathsLengths[0].path; 
 }
 
-function removeDuplicates(arr) {
-    var unique = [];
-    arr.forEach(element => {
-        if (!unique.includes(element.pathLength)) {
-            unique.push(element);
-        }
-    });
-    return unique;
-}
-
-function mergeSort (arr) {
-    const l = arr.length;
-    const m = Math.floor(l/2);
-
-    if(l==2){return merge(arr.slice(0,1),arr.slice(1))}
-    else if(l==3){return merge(mergeSort(arr.slice(0,2)),arr.slice(2))}
-    else{return merge(mergeSort(arr.slice(0,m)),mergeSort(arr.slice(m)))}
-}
-
-
-function merge (ar1,ar2) {
-    const l1 = ar1.length;
-    const l2 = ar2.length; 
-    if(l1==0){return ar2}
-    if(l2==0){return ar1}
-        
-    if(ar1[0]["pathLength"]<ar2[0]["pathLength"]){
-        return [ar1.shift()].concat(merge(ar1,ar2));
-    }
-    else{
-        return [ar2.shift()].concat(merge(ar1,ar2));
-    }
-}
-
 
 console.log("Min Path: " , minPath([0,0],[1,2]));
-console.log("Min Path: " , minPath([0,0],[5,2]));
+console.log("Min Path: " , minPath([0,0],[0,1]));
+console.log("Min Path: " , minPath([0,0],[6,3]));
